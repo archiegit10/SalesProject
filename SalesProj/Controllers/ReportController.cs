b10392ab186f65d7e3b7cde3a5f6e73b23a9e220
Archie <34271220+wardar10@users.noreply.github.com>
2021-10-26 16:54:48 +0100

    report controller

--- SalesProj/Controllers/ReportController.cs
+++ SalesProj/Controllers/ReportController.cs
@@ -16,43 +16,43 @@ namespace SalesProj.Controllers
         {
             this.productService = productService;
         }
-        public void Create()
-        {
-            Console.WriteLine("Enter the Sale ID:");
-            int saleID = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("Enter the Product Name:");
-            string productName = Console.ReadLine();
-
-            Console.WriteLine("Enter the Quantity:");
-            int quantity = Convert.ToInt32(Console.ReadLine());
-
-            Console.WriteLine("Enter the Price:");
-            double price = Convert.ToDouble(Console.ReadLine());
-
-            DateTime saleDate = DateTime.Today;
-
-            Product toCreate = new Product() { saleID = saleID, productName = productName, quantity = quantity, price = price, saleDate = saleDate };
-            Product newProduct = productService.Create(toCreate);
-            Console.WriteLine($"New product created: {newProduct}");
-
-        }
         public void ProductSalesYear()
         {
             // list all individual sales for year
+            Console.WriteLine("Enter the Year:");
+            Console.Write(">");
+            int salesYear = Convert.ToInt32(Console.ReadLine());
+
         }
         public void ProductSalesMonth()
         {
-            // list all individual sales for month
+            // list all individual sales for month and year
+            Console.WriteLine("Enter the Month:");
+            Console.Write(">");
+            int salesMonth = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the Year:");
+            Console.Write(">");
+            int salesYear = Convert.ToInt32(Console.ReadLine());
         }
 
         public void TotalSalesYear()
         {
-            // sum of all sales
+            // sum of all sales for year
+            Console.WriteLine("Enter the Year:");
+            Console.Write(">");
+            int totalYear = Convert.ToInt32(Console.ReadLine());
         }
         public void TotalSalesMonth()
         {
             // sum of sales for month
+            Console.WriteLine("Enter the Month:");
+            Console.Write(">");
+            int totalMonth = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the Year:");
+            Console.Write(">");
+            int totalYear = Convert.ToInt32(Console.ReadLine());
+
         }
     }
 }
