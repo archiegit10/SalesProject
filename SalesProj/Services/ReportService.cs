f40369f7ddc55e7703b6adecf1a6b15ef9d94035
Archie <34271220+wardar10@users.noreply.github.com>
2021-10-28 23:05:13 +0100

    messing about hoping its fixed

---
+++ SalesProj/Services/ReportService.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalesProj.Services
+{
+    class ReportService
+    {
+
+        /*
+        public ReportService(ProductRepository productRepository)
+        {
+            this.productRepository = productRepository;
+        }
+        internal string TotalYearRead(string totalYearSum, string year)
+        {
+            string sum = totalYearSum;
+            string passYear = year;
+            return productRepository.TotalYearRead(totalYearSum, passYear);
+        }
+
+        internal string TotalYearMonthRead(string totalYearMonthSum, string year, string month)
+        {
+            string sum = totalYearMonthSum;
+            string passYear = year;
+            string passMonth = month;
+            return productRepository.TotalYearMonthRead(totalYearMonthSum, passYear, passMonth);
+        }
+        internal IEnumerable<Product> TotalSalesYear(string year)
+        {
+            string passYear = year;
+            return productRepository.TotalSalesYear(passYear);
+        }
+        internal IEnumerable<Product> TotalSalesYearMonth(string year, string month)
+        {
+            string passYear = year;
+            string passMonth = month;
+            return productRepository.TotalSalesYearMonth(passYear, passMonth);
+        }
+        */
+    }
+}
